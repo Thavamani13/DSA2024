ff16c875bb3ef96b96064d7ddc377a5232b27161
Thavamani Murugan <thavamani1304@gmail.com>
2024-08-29 12:06:21 +0530

    Added two sum Problem

---
+++ src/main/java/P0001_TwoSum.java
@@ -0,0 +1,35 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class P0001_TwoSum {
+
+    //    https://leetcode.com/problems/two-sum/
+
+    @Test()
+    public void testcase1() {
+        int[] nums = new int[]{2, 7, 11, 15};
+        int target = 9;
+        Assert.assertEquals(twoSum(nums,target),new int[]{0,1});
+
+
+
+    }
+
+
+    private int[] twoSum(int[] nums, int target) {
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            int leftVal = nums[i];
+            for (int j = i + 1; j < nums.length; j++) {
+                int rightVal = nums[j];
+                if (leftVal + rightVal == target) return new int[]{i, j};
+
+
+            }
+        }
+
+        return new int[]{-1, -1};
+    }
+
+
+}
