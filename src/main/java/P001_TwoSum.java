af80e54d5ba5a892be347da09c18319321eb2a61
Thavamani Murugan <thavamani1304@gmail.com>
2024-08-30 10:43:33 +0530

    Added optimized solution

--- src/main/java/P001_TwoSum.java
+++ src/main/java/P001_TwoSum.java
@@ -1,6 +1,9 @@
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class P001_TwoSum {
 
     //    https://leetcode.com/problems/two-sum/
@@ -10,9 +13,13 @@ public class P001_TwoSum {
         int[] nums = new int[]{2, 7, 11, 15};
         int target = 9;
         Assert.assertEquals(twoSum(nums,target),new int[]{0,1});
+    }
 
-
-
+    @Test()
+    public void testcase2() {
+        int[] nums = new int[]{2, 7, 11, 15};
+        int target = 9;
+        Assert.assertEquals(twoSumUsingMap(nums,target),new int[]{0,1});
     }
 
 
@@ -30,6 +37,30 @@ public class P001_TwoSum {
 
         return new int[]{-1, -1};
     }
+
+
+    public int[] twoSumUsingMap(int[] nums, int target) {
+
+        Map<Integer,Integer> map = new HashMap();
+
+        for (int i = 0; i < nums.length ; i++) {
+            int inputVal = nums[i];
+            int difference = target-inputVal;
+
+            if(!map.containsKey(difference)){
+                map.put(inputVal,i);
+            }
+            else {
+                int indexVal = map.get(difference);
+                return new int[]{indexVal,i};
+            }
+
+        }
+
+        return new int[]{-1,1};
+    }
+
+
 
 
 }
